239378b7c03234bb37a650a21c1a8ef246ecc1a8
Yaiser <yaiserar@gmail.com>
2022-02-16 22:20:04 +0100

    feat basic getAll no services or IoC

---
+++ webAPISocial/SocialMedia.Core/Entities/PostEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SocialMedia.Core.Entities
+{
+    public class PostEntities
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ webAPISocial/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<PostEntities> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new PostEntities
+            {
+                PostId = x,
+                UserId = x * 2,
+                Description = $"Description{x}",
+                CreatedDate = DateTime.Now,
+                Image = $"https://picsum.photos/id/{x*5}/200/300",
+            });
+
+            return posts;
+
+        }
+    }
+}
