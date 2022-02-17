ba4f2e1865ce8d6a591379b555d8991d0a7311f1
Yaiser <yaiserar@gmail.com>
2022-02-17 04:50:20 +0100

    feat fix launch url

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using VY.SocialMedia.Data.Contracts.Interfaces;
 using VY.SocialMedia.Data.Implementation.Data;
 using VY.SocialMedia.Data.Implementation.Repositories;
